4593291c16a80679219c983d2e5b4844a16957cd
Alvaro Obyrne <alvarobyrne@gmail.com>
2022-09-23 11:35:35 -0500

    moved the theme object to another file

--- src/HydraEditorConfiguration.ts
+++ src/HydraEditorConfiguration.ts
@@ -21,6 +21,7 @@ import { lintKeymap /*linter*/ } from "@codemirror/lint";
 import { commentKeymap } from "@codemirror/comment";
 import hydraKeys from "./hydra-environment/keymaps.js";
 import { getLine, getBlock } from "./evalKeymaps";
+import themeObject from "./hydra-environment/theme";
 
 class HydraEditorConfiguration {
   constructor() {}
@@ -39,19 +40,6 @@ class HydraEditorConfiguration {
         self.emit(val, text);
       },
     }));
-    //@ts-ignore
-    const keymapOf = keymap.of([
-      ...closeBracketsKeymap,
-      ...defaultKeymap,
-      ...searchKeymap,
-      ...historyKeymap,
-      ...foldKeymap,
-      ...completionKeymap,
-      ...lintKeymap,
-      ...commentKeymap,
-      // ...evalKeymap
-      ...hydraKeymaps,
-    ]);
     // console.log("keymapOf: ", keymapOf);
 
     return [
@@ -108,77 +96,7 @@ class HydraEditorConfiguration {
         // ...evalKeymap
         ...hydraKeymaps,
       ]),
-      EditorView.theme({
-        "&": {
-          backgroundColor: "transparent",
-          fontSize: "16px",
-          color: "white",
-          fontFamily: `'Roboto Mono', monospace`,
-        },
-        "& .cm-scroller": {
-          fontFamily: `'Roboto Mono', monospace`,
-        },
-        "& .cm-line": {
-          maxWidth: "fit-content",
-          // background: 'hsla(50,23%,5%,0.6)',
-          background: "rgba(0, 0, 0, 0.7)",
-        },
-        "& .ͼo": {
-          color: "white",
-        },
-        "& .cm-tooltip.cm-tooltip-autocomplete > ul": {
-          minWidth: "80px",
-          fontFamily: `'Roboto Mono', monospace`,
-        },
-        "&.cm-focused": {
-          outline: "none",
-        },
-        "& .cm-tooltip": {
-          background: `rgba(0, 0, 0, 0.5)`,
-          // color: '#abb2bf'
-        },
-        "& .cm-tooltip-autocomplete > ul > li[aria-selected]": {
-          color: "white",
-          // color: '#abb2bf',
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-        },
-        ".cm-completionInfo": {
-          // fontFamily: 'monospace',
-          fontFamily: `'Roboto Mono', monospace`,
-          fontStyle: "italic",
-          // color: '#abb2bf',
-          padding: "1.5px 9px",
-        },
-        ".cm-completionIcon": {
-          width: "4px",
-          height: "10px",
-          opacity: 1,
-          paddingRight: "0px",
-          marginRight: "6px",
-        },
-        ".cm-completionIcon-src": {
-          backgroundColor: "orange",
-        },
-        ".cm-completionIcon-coord": {
-          backgroundColor: "yellow",
-        },
-        ".cm-completionIcon-color": {
-          backgroundColor: "lightgreen",
-        },
-        ".cm-completionIcon-combine": {
-          backgroundColor: "lightblue",
-        },
-        ".cm-completionIcon-combineCoord": {
-          backgroundColor: "purple",
-        },
-        ".cm-completionIcon-src:after": {
-          content: "ƒ",
-        },
-        // // adds word wrapping
-        // '.cm-content': {
-        //   whiteSpace: 'pre-wrap'
-        // }
-      }),
+      EditorView.theme(JSON.parse(JSON.stringify(themeObject))),
       oneDark,
       //  linter(esLint(new Linter()))
       // solarizedDark
